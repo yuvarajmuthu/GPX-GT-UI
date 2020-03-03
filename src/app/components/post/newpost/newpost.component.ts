593b544be913742704d6595b2d8011a398730a51
venkat <vengateshwaran.gtech@gmail.com>
2020-03-03 16:39:57 +0530

    news-feed-view-ui-changed

--- src/app/components/post/newpost/newpost.component.ts
+++ src/app/components/post/newpost/newpost.component.ts
@@ -1,124 +1,120 @@
-import { Component, OnInit, Input, Output, EventEmitter, ChangeDetectorRef } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-
+import {Component, OnInit, Input, Output, EventEmitter, ChangeDetectorRef} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {GpxFileuploadComponent} from '../../gpx-uicomponents/gpx-fileupload/gpx-fileupload.component';
-
 import {PostService} from '../../../services/post.service';
 import {DatashareService} from '../../../services/datashare.service';
-
 import {Post} from '../../../models/post';
 
 @Component({
-  selector: 'app-newpost',
-  templateUrl: './newpost.component.html',
-  styleUrls: ['./newpost.component.css']
+    selector: 'app-newpost',
+    templateUrl: './newpost.component.html',
+    styleUrls: ['./newpost.component.css']
 })
 export class NewpostComponent implements OnInit {
 
-  form: FormGroup;
+    form: FormGroup;
 
-  stagingImage:any = null;
-  public isFileStagingAreaCollapsed:boolean = true;
-
-  /*
-    public fileOverBase(e:any):void {
-      this.hasBaseDropZoneOver = e;
-    }
-   
-    public fileOverAnother(e:any):void {
-      this.hasAnotherDropZoneOver = e;
-    }
-  */
+    stagingImage: any = null;
+    public isFileStagingAreaCollapsed: boolean = true;
+    public: boolean = false;
+    friends: boolean = false;
+    onlyMe: boolean = false;
+    name: any = [];
+    icon: any = [];
     @Output() postEvent: EventEmitter<any> = new EventEmitter();
-  
-    @Input() parentPost:Post;
-    post:Post;
-    postFormData:FormData;
 
-    txtPost:string = '';
-    hideInput:boolean = false;
-    //clearInput:boolean = false;
-  
-  
-    constructor(private postService: PostService, 
-      private dataShareService:DatashareService,
-      private changeDetector : ChangeDetectorRef
-      ) {  
-        // this.form = this.fb.group({
-        //   stageUploadedImgFile: null
-        // });
+    @Input() parentPost: Post;
+    post: Post;
+    postFormData: FormData;
+
+    txtPost: string = '';
+    hideInput: boolean = false;
+
+    constructor(private postService: PostService,
+                private dataShareService: DatashareService,
+                private changeDetector: ChangeDetectorRef
+    ) {
     }
-    
+
+    Public(val) {
+        console.log(val);
+        this.public = true;
+        this.friends = false;
+        this.onlyMe = false;
+        this.name = 'Public';
+        this.icon = 'fa fa-globe';
+    }
+
+    Friends(val) {
+        console.log(val);
+        this.public = false;
+        this.friends = true;
+        this.onlyMe = false;
+        this.name = val;
+        this.icon = 'fa fa-users';
+    }
+
+    OnlyMe(val) {
+        console.log(val);
+        this.public = false;
+        this.friends = false;
+        this.onlyMe = true;
+        this.name = val;
+        this.icon = 'fa fa-user';
+    }
+
     ngOnInit() {
-      this.resetForm();
+        this.resetForm();
+        this.name = 'Public';
+        this.icon = 'fa fa-globe';
     }
 
-    resetForm(){
-      this.post = {} as Post;
-      this.postFormData = new FormData();
+    resetForm() {
+        this.post = {} as Post;
+        this.postFormData = new FormData();
     }
 
     onFileSelected(event) {
-      console.log("file object ", event);
-      let reader = new FileReader();
-//      let formData = new FormData();  
+        console.log('file object ', event);
+        let reader = new FileReader();
+        if (event.target.files && event.target.files[0]) {
+            this.postFormData.append('file', event.target.files[0]);
+            reader.readAsDataURL(event.target.files[0]);
+            reader.onload = (event) => {
+                this.stagingImage = event.target['result'];
 
-      
-      if (event.target.files && event.target.files[0]) {
 
-          //this.post.imageFile = event.target.files[0];
-          this.postFormData.append('file', event.target.files[0]);
-  //        formData.append('file', event.target.files[0]);
-  //        formData.append('file1', 'test');
-    
-          reader.readAsDataURL(event.target.files[0]);
-          reader.onload = (event) => {
-            this.stagingImage = event.target["result"]; 
-            
-
-          }
-      } 
+            };
+        }
     }
-    
+
     doTextareaValueChange(ev) {
-      try {
-        this.txtPost = ev.target.value;
-      } catch(e) {
-        console.info('could not set textarea-value');
-      }
+        try {
+            this.txtPost = ev.target.value;
+        } catch (e) {
+            console.info('could not set textarea-value');
+        }
     }
 
-    submitPost(){
-
-      this.post.entityId = this.dataShareService.getLoggedinUsername();
-      //this.post.districtId = this.dataShareService.getCurrentDistrictId();
-      this.post.postText = this.txtPost;
-  
-      if(this.parentPost != null){
-        console.log("parent post " + this.parentPost.postText + ", post id " + this.parentPost.id);
-        this.post.parentPostId = this.parentPost.id;    
-      }
-      this.postFormData.append('post', JSON.stringify(this.post));
-      this.postService.postComment(this.postFormData)
-      .subscribe((result) => {
-        //reset the new post on successful response
-        console.log("post message response " + result);
-        this.resetForm();
-
-        if(this.parentPost != null){
-          this.postEvent.emit(null);           
-   
-          this.hideInput = true; // should be inside subscribe()
-       }else{
-         this.txtPost = '';
-         this.stagingImage = null;
-         //this.changeDetector.detectChanges();
-       }  
- 
-      });
-  
-  
-  
+    submitPost() {
+        this.post.entityId = this.dataShareService.getLoggedinUsername();
+        this.post.postText = this.txtPost;
+        if (this.parentPost != null) {
+            console.log('parent post ' + this.parentPost.postText + ', post id ' + this.parentPost.id);
+            this.post.parentPostId = this.parentPost.id;
+        }
+        this.postFormData.append('post', JSON.stringify(this.post));
+        this.postService.postComment(this.postFormData)
+            .subscribe((result) => {
+                console.log('post message response ' + result);
+                this.resetForm();
+                if (this.parentPost != null) {
+                    this.postEvent.emit(null);
+                    this.hideInput = true;
+                } else {
+                    this.txtPost = '';
+                    this.stagingImage = null;
+                }
+            });
     }
-
 }
--- src/app/components/post/postcard/postcard.component.ts
+++ src/app/components/post/postcard/postcard.component.ts
@@ -1,125 +1,173 @@
 import {Component, OnInit, Input, isDevMode} from '@angular/core';
 //import { formatDate } from "@angular/common";
-
 import {PostService} from '../../../services/post.service';
 import {UserService} from '../../../services/user.service';
 import {DatashareService} from '../../../services/datashare.service';
-
 import {Post} from '../../../models/post';
 
 @Component({
-  selector: 'app-postcard',
-  templateUrl: './postcard.component.html',
-  styleUrls: ['./postcard.component.css']
+    selector: 'app-postcard',
+    templateUrl: './postcard.component.html',
+    styleUrls: ['./postcard.component.css']
 })
 
 export class PostcardComponent implements OnInit {
-  @Input() post: Post;
-  commentPost:boolean = false;
-  devMode:boolean = true;
+    @Input() post: Post;
+    commentPost: boolean = false;
+    devMode: boolean = true;
+    savePost: boolean = false;
+    hidePost: boolean = false;
+    reportPost: boolean = false;
+    liked: boolean = false;
+    name: any = '';
+    icon: any = '';
 
-  profileSmImage:any = "assets/images/avatar1.png";
-  isImageLoading:boolean = false;
-  postImage:any = "assets/images/avatar1.png";
-  isPostImageLoading:boolean = false;
-  entityId:string;
+    profileSmImage: any = 'assets/images/avatar1.png';
+    isImageLoading: boolean = false;
+    postImage: any = 'assets/images/avatar1.png';
+    isPostImageLoading: boolean = false;
+    entityId: string;
 
-  constructor(private postService: PostService, 
-    private dataShareService:DatashareService,
-    private userService:UserService) {
-      this.devMode = isDevMode();
+    constructor(private postService: PostService,
+                private dataShareService: DatashareService,
+                private userService: UserService) {
+        this.devMode = isDevMode();
 
-  }
+    }
 
-  ngOnInit(): void {
-    ////Get entity image
-    if(this.post.postType){
-      if(this.post.postType.indexOf("V") !== -1){ 
-        this.post["containsVideo"] = true;
-      }
-      if(this.post.postType.indexOf("I") !== -1){
-        this.post["containsImage"] = true;
-        if(!this.devMode){
-          this.getPostImage(this.post["relatedFiles"][0]);
+    ngOnInit(): void {
+        ////Get entity image
+        if (this.post.postType) {
+            if (this.post.postType.indexOf('V') !== -1) {
+                this.post['containsVideo'] = true;
+            }
+            if (this.post.postType.indexOf('I') !== -1) {
+                this.post['containsImage'] = true;
+                if (!this.devMode) {
+                    this.getPostImage(this.post['relatedFiles'][0]);
+                }
+                //TODO
+                //Get image
+            }
+            if (this.post.postType.indexOf('T') !== -1) {
+                this.post['containsText'] = true;
+            }
         }
-        //TODO 
-        //Get image
-      }
-      if(this.post.postType.indexOf("T") !== -1){
-        this.post["containsText"] = true;
-      }
+        this.entityId = this.dataShareService.getLoggedinUsername();
+
+        if (!this.devMode) {
+            this.getProfileSmImage(this.entityId);
+        }
     }
-    this.entityId=this.dataShareService.getLoggedinUsername(); 
 
-    if(!this.devMode){
-      this.getProfileSmImage(this.entityId);
+    save(val) {
+        console.log(val);
+        this.savePost = true;
+        this.hidePost = false;
+        this.reportPost = false;
+        this.name = val;
+        this.icon = 'fa fa-floppy-o';
     }
-  }
-  
- 
-  postEvent():void{
-    this.commentPost = false;  
-  }
 
-  comment():void{
-    this.commentPost = true;
-    //child hideInput set to false
-    console.log("this.commentPost " + this.commentPost);
-  }
-
-  likePost():void{
-    console.log('Liked the post ' + this.post.id);
-    console.log('userid ' + this.dataShareService.getCurrentUserId());
-    this.post.likedByCurrentUser = true;
-    console.log('this.post.likedByCurrentUser ' + this.post.likedByCurrentUser);
-  }  
-
-  loadMoreComments(id:string){ 
-    console.log("Loading More Post Comments for ", id);
-    return false;
-  }
-
-  getProfileSmImage(userId:string) {
-    this.isImageLoading = true;
-    this.userService.getImage(userId).subscribe(data => {
-      this.createImageFromBlob(data);
-      this.isImageLoading = false;
-    }, error => {
-      this.isImageLoading = false;
-      console.log(error);
-    });
-  } 
-
-  createImageFromBlob(image: Blob) {
-    let reader = new FileReader();
-    reader.addEventListener("load", () => {
-      this.profileSmImage = reader.result; 
-    }, false);
-  
-    if (image) {
-      reader.readAsDataURL(image);
+    like(event) {
+        let check = event.target.classList.contains('post-active');
+        if (check === true) {
+            event.target.classList.remove('post-active');
+            /*this.liked = false;
+            this.savePost = false;
+            this.hidePost = false;
+            this.reportPost = false;*/
+        } else {
+            event.target.classList.add('post-active');
+            /*this.liked = true;
+            this.savePost = false;
+            this.hidePost = false;
+            this.reportPost = false;*/
+        }
     }
-  }
 
-  getPostImage(imageId:string) {
-    this.isPostImageLoading = true;
-    this.postService.getImage(imageId).subscribe(data => {
-      this.createPostImageFromBlob(data);
-      this.isPostImageLoading = false;
-    }, error => {
-      this.isPostImageLoading = false;
-      console.log(error);
-    });
-  } 
-
-  createPostImageFromBlob(image: Blob) {
-    let reader = new FileReader();
-    reader.addEventListener("load", () => {
-      this.postImage = reader.result; 
-    }, false);
-  
-    if (image) {
-      reader.readAsDataURL(image);
+    hide(val) {
+        console.log(val);
+        this.savePost = false;
+        this.hidePost = true;
+        this.reportPost = false;
+        this.name = val;
+        this.icon = 'fa fa-eye-slash';
     }
-  }
+
+    report(val) {
+        console.log(val);
+        this.savePost = false;
+        this.hidePost = false;
+        this.reportPost = true;
+        this.name = val;
+        this.icon = 'fa fa-exclamation-triangle';
+    }
+
+
+    postEvent(): void {
+        this.commentPost = false;
+    }
+
+    comment(): void {
+        this.commentPost = true;
+        //child hideInput set to false
+        console.log('this.commentPost ' + this.commentPost);
+    }
+
+    likePost(): void {
+        console.log('Liked the post ' + this.post.id);
+        console.log('userid ' + this.dataShareService.getCurrentUserId());
+        this.post.likedByCurrentUser = true;
+        console.log('this.post.likedByCurrentUser ' + this.post.likedByCurrentUser);
+    }
+
+    loadMoreComments(id: string) {
+        console.log('Loading More Post Comments for ', id);
+        return false;
+    }
+
+    getProfileSmImage(userId: string) {
+        this.isImageLoading = true;
+        this.userService.getImage(userId).subscribe(data => {
+            this.createImageFromBlob(data);
+            this.isImageLoading = false;
+        }, error => {
+            this.isImageLoading = false;
+            console.log(error);
+        });
+    }
+
+    createImageFromBlob(image: Blob) {
+        let reader = new FileReader();
+        reader.addEventListener('load', () => {
+            this.profileSmImage = reader.result;
+        }, false);
+
+        if (image) {
+            reader.readAsDataURL(image);
+        }
+    }
+
+    getPostImage(imageId: string) {
+        this.isPostImageLoading = true;
+        this.postService.getImage(imageId).subscribe(data => {
+            this.createPostImageFromBlob(data);
+            this.isPostImageLoading = false;
+        }, error => {
+            this.isPostImageLoading = false;
+            console.log(error);
+        });
+    }
+
+    createPostImageFromBlob(image: Blob) {
+        let reader = new FileReader();
+        reader.addEventListener('load', () => {
+            this.postImage = reader.result;
+        }, false);
+
+        if (image) {
+            reader.readAsDataURL(image);
+        }
+    }
 }

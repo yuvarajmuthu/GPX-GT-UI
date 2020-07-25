7ad5cd17ba9926949e13d47162e2a4dc36763909
yuvaraj.muthu <yuvaraj.muthu@gmail.com>
2020-07-25 12:02:57 -0400

    moved browser module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import {BrowserModule} from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { ScrollingModule } from '@angular/cdk/scrolling';
@@ -101,6 +102,7 @@ let config = new AuthServiceConfig([
     ],
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
         DeviceDetectorModule,
         AppRoutingModule,
         HttpClientModule,

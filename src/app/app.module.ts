a148c00c6b5b562719b11f9ebf5b3f12f5cd2a3b
muthu48 <muthugrandpa@gmail.com>
2021-12-17 19:10:58 -0500

    new release 12172021

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -130,12 +130,12 @@ let config = new AuthServiceConfig([
                 tokenGetter: tokenGetter,
                 //Authenticated requests should only be sent to whitelistedDomains
                 //DEV mode
-                whitelistedDomains: ['localhost:5000'],
+                //whitelistedDomains: ['localhost:5000'],
                 //PROD mode
-                //whitelistedDomains: ['www.gpxservice.xyz'],
+                whitelistedDomains: ['www.gpxservice.xyz'],
                 //specific routes that shouldn’t receive the JWT even if they are on a whitelisted domain
-                blacklistedRoutes: ['localhost:5000/login','localhost:5000/user/tokenVerify']
-                //blacklistedRoutes: ['https://www.gpxservice.xyz/login','https://www.gpxservice.xyz/user/tokenVerify']
+                //blacklistedRoutes: ['localhost:5000/login','localhost:5000/user/tokenVerify']
+                blacklistedRoutes: ['https://www.gpxservice.xyz/login','https://www.gpxservice.xyz/user/tokenVerify']
             }
         }),
         AngularFontAwesomeModule, //OBSOLETE
@@ -155,13 +155,13 @@ let config = new AuthServiceConfig([
       UserModule],
     providers: [
       //enable the interceptor only for DEV mode
-      
+      /*
         {
             provide: HTTP_INTERCEPTORS,
             useClass: MockHttpInterceptorService,
             multi: true
         },
-        
+        */
         
         AuthenticationService,
         AuthGuard,
